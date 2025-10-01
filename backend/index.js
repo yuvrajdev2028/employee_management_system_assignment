636cc878ecbdf993c6d6ba308d27d6f09e92b595
Yuvraj Pratap Singh <yuvraj.singh@biz2x.com>
2025-10-01 17:44:33 +0530

    configured port

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const dbConnect = require('./config/databaseConfig');
+require('dotenv').config();
 
 const app = express();
 
-PORT = 4000;
+PORT = process.env.PORT|5000;
 
 app.get("/",(req,res)=>{
     res.send("<h2>Server started...</h2>")
