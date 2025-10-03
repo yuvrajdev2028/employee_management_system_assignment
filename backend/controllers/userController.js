cd3ea89f920d8e87317007a2aa6899c75787a2c0
Yuvraj Pratap Singh <yuvraj.singh@biz2x.com>
2025-10-03 12:47:13 +0530

    finished with authentication setup and functionality to create user and remove user accessible only by admin

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -5,12 +5,36 @@ const { generateAccessToken, generateRefreshToken } = require('../utils/tokenUti
 
 exports.createUser = async(req,res)=>{
     try{
-        // const { name, password, }
+        const { name, companyEmail, password, role} = req.body;
+        const hashedPassword = await hashPassword(password);
+        if(!hashedPassword) throw new Error("Password not hashed");
+        const newUser = new User({name,companyEmail,password:hashedPassword,role});
+        await newUser.save();
+        res.status(200).json({
+            message:"New user created successfully."
+        })
     }
     catch(error){
         console.log("Error occured while creating user: ", error);
         res.status(500).json({
             message:"Error occured while creating user."
+        })
+    }
+}
+
+exports.removeUser=async(req,res)=>{
+    try{
+        const id = req.params.id;
+        if(!id) throw new Error("ID not found.");
+        await User.deleteOne({_id:id});
+        res.status(200).json({
+            message:"User removed successfully."
+        })
+    }
+    catch(error){
+        console.log("Error occured while removing user:",error)
+        res.status(500).json({
+            message:"Error occured on server while removing user."
         })
     }
 }
@@ -58,7 +82,7 @@ exports.login = async(req,res)=>{
 
 exports.logout = async(req,res)=>{
     try{
-        res.clearCookie('token',{
+        res.clearCookie('refreshToken',{
             httpOnly: true,
             secure: true,
             sameSite: 'Strict'
