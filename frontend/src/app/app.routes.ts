ce5f1b25f85304a691226610462ea3f568a3c74f
yuvrajdev1920 <yuvraj.pratap1920@gmail.com>
2025-10-04 01:32:50 +0530

    setup frontend with tailwind css

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { LoginPage } from './pages/login-page/login-page';
+import { DashboardPage } from './pages/dashboard-page/dashboard-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'dashboard', component: DashboardPage, },
+    { path: 'login', component: LoginPage },
+    { path: '', redirectTo: 'login', pathMatch: 'full' }
+];
